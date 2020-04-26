d2d03202e2f3e9d3ccb62f74d1f437dcb9fa213b
killinq-joke <zakadmaster@gmail.com>
2020-04-26 04:17:46 +0200

    Addition of GET / tests

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -1,11 +1,25 @@
-
+const request = require("supertest");
+const server = require("../api/server");
 
 describe("server.js", () => {
-    it("sanity check", () => {
-        expect(5).toBe(5)
-    })
+  it("sanity check", () => {
+    expect(5).toBe(5);
+  });
 
-    describe("GET /", () => {
-        
-    })
-})
+  describe("GET /", () => {
+    it("returns 200 OK", async () => {
+      await request(server)
+        .get("/")
+        .then((res) => {
+          expect(200);
+        });
+    });
+    it("returns {api: 'is up'}", async () => {
+      await request(server)
+        .get("/")
+        .then((res) => {
+          expect(res.body).toEqual({api: 'is up'});
+        });
+    });
+  });
+});
