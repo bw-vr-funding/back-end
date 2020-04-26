bc653f15e3993f3d26cf7396f5f0a12351fb5e17
killinq-joke <zakadmaster@gmail.com>
2020-04-27 01:12:04 +0200

    Addition of initialMigration layout

--- data/migrations/20200426035517_initialMigration.js
+++ data/migrations/20200426035517_initialMigration.js
@@ -1,8 +1,45 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", (tbl) => {
+      tbl.increments();
+      tbl.string("username", 24).notNullable().unique();
+      tbl.string("password").notNullable();
+      tbl.string("email").notNullable();
+    })
+    .createTable("projects", (tbl) => {
+      tbl.increments();
+      tbl.string("name", 40).notNullable().unique();
+      tbl.string("description").notNullable();
+      tbl.string("img_url").notNullable();
+      tbl.string("category", 16).notNullable();
+      tbl.integer("funding_goal").notNullable();
+      tbl.integer("funding").notNullable();
+      tbl
+        .integer("creator_id")
+        .unsigned()
+        .references("id")
+        .inTable("users");
+    })
+    .createTable("users-projects", (tbl) => {
+      tbl.increments();
+      tbl
+        .integer("funder_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users");
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users");
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("users-projects")
+    .dropTableIfExists("projects")
+    .dropTableIfExists("users");
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
-require("dotenv").config()
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
