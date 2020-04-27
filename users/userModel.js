2c30664cd6f5096b06ac61bea9d529e3598d2923
killinq-joke <zakadmaster@gmail.com>
2020-04-27 20:37:07 +0200

    Addition of del() & update()

--- users/userModel.js
+++ users/userModel.js
@@ -3,6 +3,8 @@ const db = require("../data/dbConfig");
 module.exports = {
   findBy,
   add,
+  del,
+  update,
 };
 
 function findBy(filter) {
@@ -11,4 +13,12 @@ function findBy(filter) {
 
 function add(user) {
   return db("users").insert(user);
+}
+
+function del(id) {
+  return db("users").del().where({ id });
+}
+
+function update(id, changes) {
+  return db("users").update(changes).where({ id });
 }
