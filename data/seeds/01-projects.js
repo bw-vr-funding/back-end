08bfda6cfd861d5ab04f1edf8bb5c32f3be906a4
killinq-joke <zakadmaster@gmail.com>
2020-04-28 00:06:40 +0200

    Ran projects seed

--- data/seeds/01-projects.js
+++ data/seeds/01-projects.js
@@ -1,13 +1,21 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('projects').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("projects").insert([
+        {
+          id: 1,
+          name: "loup-garou pour une nuit",
+          description: "One of the most loved french party game ever created",
+          img_url:
+            "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTcvaksBQhrVCyZL4yvLy6WhAcg1GN9mC9tNdganvq8zKAWMIpk-QQ&usqp=CAc",
+          category: "game",
+          funding_goal: 200000,
+          funding: 163290,
+          creator_id: 1,
+        },
       ]);
     });
 };
