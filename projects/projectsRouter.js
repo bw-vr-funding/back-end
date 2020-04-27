6546c0e84873ddb5be887dec666073620c62f4b2
killinq-joke <zakadmaster@gmail.com>
2020-04-27 19:59:57 +0200

    Addition of POST '/'

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -2,13 +2,25 @@ const router = require("express").Router();
 const md = require("./projectsModel");
 
 router.get("/", (req, res) => {
-    md.getAll()
-    .then(projects => {
-        res.json({projects})
+  md.getAll()
+    .then((projects) => {
+      res.json({ projects });
     })
-    .catch(err => {
-        res.status(500).json({errorMessage: "server error"})
+    .catch((err) => {
+      res.status(500).json({ errorMessage: "server error" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const project = req.body;
+
+  md.add(project)
+    .then((added) => {
+      res.status(201).json({ added });
     })
+    .catch((err) => {
+      res.status(500).json({ errorMessage: "server error" });
+    });
 });
 
 module.exports = router;
