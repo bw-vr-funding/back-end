19ec8a5e1a15c4e08deeeb46488bca476f4ac4be
killinq-joke <zakadmaster@gmail.com>
2020-04-27 20:09:37 +0200

    Addition of DELETE & PUT

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -23,4 +23,29 @@ router.post("/", (req, res) => {
     });
 });
 
+router.put("/:id", (req, res) => {
+  const project = req.body;
+  const { id } = req.params;
+
+  md.update(id, project)
+    .then((added) => {
+      res.status(201).json({ added });
+    })
+    .catch((err) => {
+      res.status(500).json({ errorMessage: "server error" });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  md.del(id)
+    .then((deleted) => {
+      res.status(200).json({ deleted });
+    })
+    .catch((err) => {
+      res.status(500).json({ errorMessage: "server error" });
+    });
+});
+
 module.exports = router;
