bbe7d8045383655ad44706fd4c77139e12ca61a6
killinq-joke <zakadmaster@gmail.com>
2020-04-28 00:29:45 +0200

    Fixed imports in server.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const authRouter = require("../auth/auth-router");
-const usersRouter = require("../users/userRouter");
 const projectsRouter = require("../projects/projectsRouter");
 
 
@@ -12,7 +11,6 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use("/auth", authRouter);
-server.use("/users", usersRouter);
 server.use("/projects", projectsRouter);
 
 server.get("/", (req, res) => {
