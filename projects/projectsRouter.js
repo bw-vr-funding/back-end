4141fc001a85ae46bed79ac860cb96ea534c37e3
killinq-joke <zakadmaster@gmail.com>
2020-04-27 19:55:57 +0200

    Addition of GET / to get all

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
+const md = require("./projectsModel");
+
+router.get("/", (req, res) => {
+    md.getAll()
+    .then(projects => {
+        res.json({projects})
+    })
+    .catch(err => {
+        res.status(500).json({errorMessage: "server error"})
+    })
+});
 
 module.exports = router;
