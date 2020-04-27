de1ff844f35261ca03888bf46085974034d73743
killinq-joke <zakadmaster@gmail.com>
2020-04-27 19:35:35 +0200

    Addition of getAll() to projectsModel

--- projects/projectsModel.js
+++ projects/projectsModel.js
@@ -1,10 +1,13 @@
 const db = require("../data/dbConfig");
 
 module.exports = {
+    getAll,
   add,
 };
 
-
+function getAll() {
+  return db("projects");
+}
 
 function add(project) {
   return db("projects").insert(project);
