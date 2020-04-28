c062563df884d4e5285fe5839db9362fb70ffeb7
killinq-joke <zakadmaster@gmail.com>
2020-04-28 17:39:16 +0200

    Addition of foreign key constraints & 1 test of POST /projects

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -4,6 +4,7 @@ const db = require("../data/dbConfig");
 
 beforeEach(async () => {
   await db("users").truncate();
+  await db("projects").truncate();
 });
 
 describe("server.js", () => {
@@ -73,6 +74,41 @@ describe("GET /projects", () => {
               .get("/projects")
               .set("Authorization", token)
               .expect(200);
+          });
+      });
+  });
+});
+
+describe("POST /projects", () => {
+  const project = {
+          id: 1,
+          name: "loup-garou pour une nuit",
+          description: "One of the most loved french party game ever created",
+          img_url:
+            "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTcvaksBQhrVCyZL4yvLy6WhAcg1GN9mC9tNdganvq8zKAWMIpk-QQ&usqp=CAc",
+          category: "game",
+          funding_goal: 200000,
+          funding: 163290,
+          creator_id: 1,
+        }
+  it("returns 401 Unauthorized w/out token", async () => {
+    await request(server).post("/projects").send(project).expect(401);
+  });
+  it("returns 200 OK w/ token", async () => {
+    await request(server)
+      .post("/auth/register")
+      .send({ username: "test", password: "projects" })
+      .then(async () => {
+        await request(server)
+          .post("/auth/login")
+          .send({ username: "test", password: "projects" })
+          .then( (res) => {
+            const token = res.body.token;
+            return request(server)
+              .post("/projects")
+              .set("Authorization", token)
+              .send(project)
+              .expect(201);
           });
       });
   });
