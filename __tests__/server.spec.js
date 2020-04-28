26a007432154e2c95ca021e2543807cd435e95cd
killinq-joke <zakadmaster@gmail.com>
2020-04-28 03:15:57 +0200

    Addition of some login & register tests

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -1,5 +1,10 @@
 const request = require("supertest");
 const server = require("../api/server");
+const db = require("../data/dbConfig");
+
+beforeEach(async () => {
+  await db("users").truncate();
+});
 
 describe("server.js", () => {
   it("sanity check", () => {
@@ -15,6 +20,36 @@ describe("server.js", () => {
         .get("/")
         .then((res) => {
           expect(res.body).toEqual({ api: "is up" });
+        });
+    });
+  });
+});
+
+describe("auth-router.js", () => {
+  describe("POST /auth/register", () => {
+    it("returns 201 created", async () => {
+      await request(server)
+        .post("/auth/register")
+        .send({ username: "user", password: "created" })
+        .expect(201);
+    });
+  });
+  describe("POST /auth/login", () => {
+    it("returns 401 Unauthorized without right credentials", async () => {
+      await request(server)
+        .post("/auth/login")
+        .send({ username: "wrong", password: "credentials" })
+        .expect(401);
+    });
+    it("returns 200 OK with right credentials", async () => {
+      await request(server)
+        .post("/auth/register")
+        .send({ username: "right", password: "credentials" })
+        .then(async () => {
+          await request(server)
+            .post("/auth/login")
+            .send({ username: "right", password: "credentials" })
+            .expect(200);
         });
     });
   });
